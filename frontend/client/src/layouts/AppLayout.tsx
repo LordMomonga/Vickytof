6f74b910170b404250be51ef99fd3a3aae2b2f8e
LordMomonga <kamisisamuel28@gmail.com>
2026-03-08 14:44:53 -0400

    mise ajout

--- frontend/client/src/layouts/AppLayout.tsx
+++ frontend/client/src/layouts/AppLayout.tsx
@@ -130,7 +130,7 @@ export const AppLayout = () => {
                             </IconButton>
 
 
-              <Link to="/" className="brand-script ml-10 text-2xl text-violet-500">
+              <Link to="/" className="brand-script ml-10 text-2xl text-violet-500 font-bold">
                 vicktykof
               </Link>
             </div>
